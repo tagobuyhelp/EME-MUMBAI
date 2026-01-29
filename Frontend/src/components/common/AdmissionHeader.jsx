e2b36774fd38f847adb3ee31302c446869e40a2f
Tagobuy <tagobuyhelp@gmail.com>
2026-01-29 17:58:14 +0530

    Fix Building Error

--- Frontend/src/components/common/AdmissionHeader.jsx
+++ Frontend/src/components/common/AdmissionHeader.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { ArrowRight, ChevronRight } from "lucide-react";
 import { Button } from "../ui/button";
