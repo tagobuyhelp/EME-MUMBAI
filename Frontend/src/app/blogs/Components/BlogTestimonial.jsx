60099f7c4d644d3b64637b6c5180cdf64aaf505b
Tagobuy <tagobuyhelp@gmail.com>
2026-07-25 11:18:07 +0530

    ...

--- Frontend/src/app/blogs/Components/BlogTestimonial.jsx
+++ Frontend/src/app/blogs/Components/BlogTestimonial.jsx
@@ -1,8 +1,9 @@
 import CTAButton from "@/components/common/CtaButton";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import Marquee from "react-fast-marquee";
+import { alumniStudents } from "@/data/alumniStudentsData";
 
 export default function BlogTestimonial({ _this }) {
   return (
@@ -14,87 +15,32 @@ export default function BlogTestimonial({ _this }) {
         <strong>“Salary Ranges up to ₹40,000 per month.”</strong>
       </p>
       <Marquee autoFill className=" px-4 pb-2  gap-5">
-        <Card className="w-[150px] md:w-[180px] rounded-[18px] border bg-white flex flex-col justify-center items-center gap-2 p-4 mx-4">
-          <Image
-            className="rounded-full w-[50px] h-[50px] object-cover object-right-top"
-            width={50}
-            height={50}
-            src="https://eme25.s3.ap-south-1.amazonaws.com/assets/images/AitrikBanerjee.webp"
-            alt="Full Stack Development"
-          />
-          <div className="text-[14px] font-[400] text-center">
-            <p className="font-[600]">Aitrik Banerjee</p>
-            {/* <p className="text-[12px] px-8 md:px-0">SAP MM Consultant</p> */}
-          </div>
-          <Image
-            className=""
-            width={80}
-            height={80}
-            src="/assets/images/TcsLogo.svg"
-            alt="Full Stack Development"
-          />
-        </Card>
-        <Card className="w-[150px] md:w-[180px] rounded-[18px] border bg-white flex flex-col justify-center items-center gap-2 p-4 mx-4">
-          <Image
-            className="rounded-full w-[50px] h-[50px] object-cover object-right-top border"
-            width={50}
-            height={50}
-            src="https://eme25.s3.ap-south-1.amazonaws.com/assets/images/sap_landing/profileTwo.png"
-            alt="profileTwo"
-          />
-          <div className="text-[14px] font-[400] text-center">
-            <p className="font-[600]">Rahul Rana</p>
-            {/* <p className="text-[12px]">SAP ABAP Consultant</p> */}
-          </div>
-          <Image
-            className=""
-            width={80}
-            height={80}
-            src="/assets/images/TcsLogo.svg"
-            alt="Full Stack Development"
-          />
-        </Card>
-        <Card className="w-[150px] md:w-[180px] rounded-[18px] border bg-white flex flex-col justify-center items-center gap-2 p-4 mx-4">
-          <Image
-            className="rounded-full w-[50px] h-[50px] object-cover object-right-top border"
-            width={50}
-            height={50}
-            src="https://eme25.s3.ap-south-1.amazonaws.com/assets/images/sap_landing/profileThree.png"
-            alt="profileTwo"
-          />
-          <div className="text-[14px] font-[400] text-center">
-            <p className="font-[600]">Rahul Singh</p>
-            {/* <p className="text-[12px]">SAP ABAP Consultant</p> */}
-          </div>
-          <Image
-            className=""
-            width={80}
-            height={80}
-            src="/assets/images/TcsLogo.svg"
-            alt="Full Stack Development"
-          />
-        </Card>
-
-        <Card className="w-[150px] md:w-[180px] rounded-[18px] border bg-white flex flex-col justify-center items-center gap-2 p-4 mx-4">
-          <Image
-            className="rounded-full w-[50px] h-[50px] object-cover object-right-top border"
-            width={50}
-            height={50}
-            src="https://eme25.s3.ap-south-1.amazonaws.com/assets/images/sap_landing/profileFour.png"
-            alt="profileTwo"
-          />
-          <div className="text-[14px] font-[400] text-center">
-            <p className="font-[600]">Probhat Routh</p>
-            {/* <p className="text-[12px] px-8 md:px-0">SAP MM Consultant</p> */}
-          </div>
-          <Image
-            className=""
-            width={80}
-            height={80}
-            src="/assets/images/TcsLogo.svg"
-            alt="Full Stack Development"
-          />
-        </Card>
+        {alumniStudents.map((student) => (
+          <Card
+            key={student.id}
+            className="w-[150px] md:w-[180px] rounded-[18px] border bg-white flex flex-col justify-center items-center gap-2 p-4 mx-4"
+          >
+            <Image
+              className="rounded-full w-[50px] h-[50px] object-cover object-right-top border"
+              width={50}
+              height={50}
+              src={student.img}
+              alt={student.title}
+            />
+            <div className="text-[14px] font-[400] text-center">
+              <p className="font-[600]">{student.title}</p>
+            </div>
+            {student.companyLogo && (
+              <Image
+                className="object-contain max-h-[35px] w-auto"
+                width={80}
+                height={40}
+                src={student.companyLogo}
+                alt={`${student.title} company logo`}
+              />
+            )}
+          </Card>
+        ))}
       </Marquee>
 
       <CTAButton
